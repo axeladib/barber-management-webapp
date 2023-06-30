cc4674029f519a12eb34cb408a21eeb97d9f0033
axeladib <nabil298adib@gmail.com>
2023-06-30 17:27:42 +0800

    Setup the postgresql db model

---
+++ app/models/role.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, Sequelize) => {
+  const Role = sequelize.define("roles", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+  });
+  return Role;
+};
+
+//These Sequelize Models represents users & roles table in PostgreSQL database.

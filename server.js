9784a6c7e56011dfa36bed32da824d6327122206
axeladib <nabil298adib@gmail.com>
2023-07-02 17:49:50 +0800

    initialize the sequelize model

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const db = require("app/models/db");
+const Role = db.role;
+
 const app = express();
 
 let corsOptions = {
-    origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 //help communication between the React App and the Express server.
 app.use(cors(corsOptions));
@@ -14,26 +17,54 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 //TODO: Parse request of content-type : x-www-form-urlencoded application
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //TODO: Simple route
-app.get("/", (request, response)=>{
-    response.json({message: "Server setup complete at 8080 port"})
+app.get("/", (request, response) => {
+  response.json({ message: "Server setup complete at 8080 port" });
 });
 
-app.post("/signup",(request, response)=>{
-    // Process the data from request.body
-    // Perform actions like saving data to the databased 
-    // Send back a response using res.send() or res.json()
-    const {username,email,phone,password} = request.body;
-    //Validate the data received then store in the database
-    console.Console.log(username)
-})
+app.post("/signup", (request, response) => {
+  // Process the data from request.body
+  // Perform actions like saving data to the databased
+  // Send back a response using res.send() or res.json()
+  const { username, email, phone, password } = request.body;
+  //Validate the data received then store in the database
+  console.log(username, email, phone, password);
+});
 
+//TODO: Initialise the Sequelize model
+
+db.sequelize.sync(
+  { force: true }.then(() => {
+    console.log("Drop and Resync Database");
+    initial();
+  })
+);
+const initial = () => {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 1,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 1,
+    name: "admin",
+  });
+};
+//Overall, this code sets up an Express application, 
+//synchronizes the database schema, and creates initial roles using Sequelize. 
+//It provides a way to initialize the database with predefined roles, ensuring 
+//they are available for use in the application.
 
 //TODO: Set port, listen to request
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
